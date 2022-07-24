51d1a3bbe35fb6c5bac8476a8319aea070b7cd73
Fabrice N'DRI <nyf005@gmail.com>
2022-07-24 12:31:11 +0000

    Harcode html content

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Todo App",
+      title: "Task Manager",
       filename: "index.html",
       template: "src/template.html",
     }),
